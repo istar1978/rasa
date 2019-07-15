32a2438ee0a451627911a4c47b7c3f1898592145
samsucik <s.sucik@rasa.com>
2019-07-15 17:36:48 +0200

    Start applying weight pruning (inspired by the State of Sparsity code) to our BERT.

--- rasa/nlu/classifiers/bert_intent_classifier.py
+++ rasa/nlu/classifiers/bert_intent_classifier.py
@@ -37,6 +37,10 @@ class BertIntentClassifier(Component):
 
     provides = ["intent", "intent_ranking"]
 
+    hidden_size = 768
+    num_hidden_layers = 12
+    num_attention_heads = 12
+
     defaults = {
         "batch_size": 64,
         "epochs": 2,
@@ -45,10 +49,13 @@ class BertIntentClassifier(Component):
         "warmup_proportion": 0.1,
         "save_checkpoints_steps": 1000,
         "save_summary_steps": 500,
-        "bert_tfhub_module_handle": "https://tfhub.dev/google/bert_uncased_L-12_H-768_A-12/1",
+        "bert_tfhub_module_handle": "https://tfhub.dev/google/bert_uncased_L-{}_H-{}_A-{}/1".format(
+            num_hidden_layers, hidden_size, num_attention_heads
+        ),
         "pretrained_model_dir": None,
         "checkpoint_dir": "./tmp/bert",
         "checkpoint_remove_before_training": True,
+        "use_tflite": False,
     }
 
     def _load_bert_params(self, config: Dict[Text, Any]) -> None:
@@ -57,7 +64,6 @@ class BertIntentClassifier(Component):
 
         if self.pretrained_model_dir:
             dir_files = os.listdir(self.pretrained_model_dir)
-            print(dir_files)
             if all(file not in dir_files for file in ("bert_config.json", "vocab.txt")):
                 logger.warning(
                     "Pretrained model dir configured as '{}' "
@@ -95,6 +101,7 @@ class BertIntentClassifier(Component):
         session: Optional["tf.Session"] = None,
         label_list: Optional[np.ndarray] = None,
         predict_fn: Optional["Predictor"] = None,
+        use_tflite=False,
     ) -> None:
         super(BertIntentClassifier, self).__init__(component_config)
 
@@ -121,6 +128,8 @@ class BertIntentClassifier(Component):
             )
 
         self.estimator = None
+
+        self.use_tflite = use_tflite
 
     def train(self, training_data, cfg, **kwargs):
         """Train this component."""
@@ -153,7 +162,7 @@ class BertIntentClassifier(Component):
         train_features = convert_examples_to_features(
             train_examples, self.label_list, self.max_seq_length, self.tokenizer
         )
-        
+
         """
         # creating small representative dataset for full post-training quantisation with TFLite
         calibration_data = []
@@ -195,11 +204,18 @@ class BertIntentClassifier(Component):
         )
 
         self.estimator = tf.estimator.Estimator(
-            model_fn=model_fn, 
-            config=run_config, 
+            model_fn=model_fn,
+            config=run_config,
             params={"batch_size": self.batch_size},
-            model_dir=self.checkpoint_dir
+            model_dir=self.checkpoint_dir,
         )
+
+        # graph = tf.get_default_graph()
+        # print(g)
+        # print(g.get_collection(tf.GraphKeys.GLOBAL_VARIABLES))
+        # writer = tf.summary.FileWriter(logdir="tfgraph-bert", graph=g)
+        # writer.flush()
+        # exit(0)
 
         # Start training
         self.estimator.train(input_fn=train_input_fn, max_steps=num_train_steps)
@@ -224,33 +240,40 @@ class BertIntentClassifier(Component):
         # Get first index since we are only classifying text blob at a time.
         example = predict_features[0]
 
-        use_tflite = True
-        
         start = time.time()
-        if use_tflite:
-            input_ids = np.array(example.input_ids).reshape(-1, self.max_seq_length).astype(np.int32)
-            self.interpreter.set_tensor(
-                self.in_indices["input_ids"], 
-                input_ids)
+        if self.use_tflite:
+            input_ids = (
+                np.array(example.input_ids)
+                .reshape(-1, self.max_seq_length)
+                .astype(np.int32)
+            )
+            self.interpreter.set_tensor(self.in_indices["input_ids"], input_ids)
 
-            input_mask = np.array(example.input_mask).reshape(-1, self.max_seq_length).astype(np.int32)
-            self.interpreter.set_tensor(
-                self.in_indices["input_mask"], 
-                input_mask)
+            input_mask = (
+                np.array(example.input_mask)
+                .reshape(-1, self.max_seq_length)
+                .astype(np.int32)
+            )
+            self.interpreter.set_tensor(self.in_indices["input_mask"], input_mask)
 
             label_ids = np.array(example.label_id).reshape(-1).astype(np.int32)
-            self.interpreter.set_tensor(
-                self.in_indices["label_ids"], 
-                label_ids)
+            self.interpreter.set_tensor(self.in_indices["label_ids"], label_ids)
 
-            segment_ids = np.array(example.segment_ids).reshape(-1, self.max_seq_length).astype(np.int32)
-            self.interpreter.set_tensor(
-                self.in_indices["segment_ids"], 
-                segment_ids)                
+            segment_ids = (
+                np.array(example.segment_ids)
+                .reshape(-1, self.max_seq_length)
+                .astype(np.int32)
+            )
+            self.interpreter.set_tensor(self.in_indices["segment_ids"], segment_ids)
 
             self.interpreter.invoke()
             result = self.interpreter.get_tensor(self.out_index)
-            probabilities = list(np.exp(result[0]))        
+            probabilities = list(np.exp(result[0]))
+            # size compression: from 408mb to around 110mb in all cases where optimisation is used, without opt it's ~437mb
+            # full:               116s (0.118s/message). micro f1: 0.884, macro f1: 0.921
+            # tflite, no optims:  597s (0.605s/message). micro f1: 0.884, macro f1: 0.921
+            # tflite, size:       1961s (1.99s/message). micro f1: 0.880, macro f1: 0.918
+            # tflite, latency:    2040s (2.07s/message). micro f1: 0.880, macro f1: 0.918
         else:
             result = self.predict_fn(
                 {
@@ -266,13 +289,8 @@ class BertIntentClassifier(Component):
                     ),
                 }
             )
-            probabilities = list(np.exp(result["probabilities"])[0])        
-        print("inference time: {:.3f}".format(time.time() - start))
-        # size compression: from 408mb to around 110mb in all cases where optimisation is used, without opt it's ~437mb
-        # full:               116s (0.118s/message). micro f1: 0.884, macro f1: 0.921
-        # tflite, no optims:  597s (0.605s/message). micro f1: 0.884, macro f1: 0.921
-        # tflite, size:       1961s (1.99s/message). micro f1: 0.880, macro f1: 0.918
-        # tflite, latency:    2040s (2.07s/message). micro f1: 0.880, macro f1: 0.918
+            probabilities = list(np.exp(result["probabilities"])[0])
+        print ("inference time: {:.3f}".format(time.time() - start))
 
         index = np.argmax(probabilities)
         label = self.label_list[index]
@@ -306,8 +324,7 @@ class BertIntentClassifier(Component):
             if e.errno != errno.EEXIST:
                 raise
         model_path = self.estimator.export_saved_model(
-            model_dir, 
-            serving_input_fn_builder(self.max_seq_length)
+            model_dir, serving_input_fn_builder(self.max_seq_length)
         )
 
         with io.open(os.path.join(model_dir, self.name + "_label_list.pkl"), "wb") as f:
@@ -324,77 +341,96 @@ class BertIntentClassifier(Component):
         cached_component: Optional["EmbeddingIntentClassifier"] = None,
         **kwargs: Any
     ) -> "BertIntentClassifier":
-        tflite_model_file = "tflite/converted_model_bert.tflite"
-        
+
         if model_dir and meta.get("model_path"):
-            
+
             with io.open(
                 os.path.join(model_dir, cls.name + "_label_list.pkl"), "rb"
             ) as f:
                 label_list = pickle.load(f)
 
-            model_path = os.path.join(model_dir, [p for p in os.walk(model_dir)][0][1][0])
+            model_path = os.path.join(
+                model_dir, [p for p in os.walk(model_dir)][0][1][0]
+            )
 
             graph = tf.Graph()
             with graph.as_default():
                 sess = tf.Session()
                 predict_fn = predictor.from_saved_model(model_path)
 
-                in_tensor_names = ["input_ids", "input_mask", "label_ids", "segment_ids"]
+                if meta["use_tflite"]:
+                    tflite_model_file = "tflite/converted_model_bert.tflite"
+                    in_tensor_names = [
+                        "input_ids",
+                        "input_mask",
+                        "label_ids",
+                        "segment_ids",
+                    ]
 
-                in_tensors = [predict_fn.feed_tensors[name] for name in in_tensor_names]
-                out_tensors = [t for _, t in predict_fn.fetch_tensors.items()]
-                converter = tf.lite.TFLiteConverter.from_session(
-                    predict_fn.session, in_tensors, out_tensors
-                )
-                
-                # """
-                converter.optimizations = [
-                    tf.lite.Optimize.DEFAULT # 4s (~219it/s) (4.604s invoking time)
-                    # tf.lite.Optimize.OPTIMIZE_FOR_SIZE # 4s (~220it/s) (4.707s invoking time)
-                    # tf.lite.Optimize.OPTIMIZE_FOR_LATENCY # 4s (~215it/s) (4.630s invoking time)
-                ]
-                # """
+                    in_tensors = [
+                        predict_fn.feed_tensors[name] for name in in_tensor_names
+                    ]
+                    out_tensors = [t for _, t in predict_fn.fetch_tensors.items()]
+                    converter = tf.lite.TFLiteConverter.from_session(
+                        predict_fn.session, in_tensors, out_tensors
+                    )
 
+                    # """
+                    converter.optimizations = [
+                        tf.lite.Optimize.DEFAULT  # 4s (~219it/s) (4.604s invoking time)
+                        # tf.lite.Optimize.OPTIMIZE_FOR_SIZE # 4s (~220it/s) (4.707s invoking time)
+                        # tf.lite.Optimize.OPTIMIZE_FOR_LATENCY # 4s (~215it/s) (4.630s invoking time)
+                    ]
+                    # """
 
-                """
-                # representative dataset: generator, each element is a list of input items, 
-                # items are passed to in_tensors in the order established when creating the converter
-                # https://github.com/tensorflow/tensorflow/blob/d883916ee45f3af81e81eefb7e8495d9fab6d231/tensorflow/lite/python/optimize/calibration_wrapper.cc#L110
+                    """
+                    # representative dataset: generator, each element is a list of input items, 
+                    # items are passed to in_tensors in the order established when creating the converter
+                    # https://github.com/tensorflow/tensorflow/blob/d883916ee45f3af81e81eefb7e8495d9fab6d231/tensorflow/lite/python/optimize/calibration_wrapper.cc#L110
 
-                with open('data/calibration_data.pickle', 'rb') as handle:
-                    calibration_data = pickle.load(handle)
+                    with open('data/calibration_data.pickle', 'rb') as handle:
+                        calibration_data = pickle.load(handle)
 
-                def representative_dataset_gen():
-                    for d in calibration_data[:5]:
-                        yield d
-                converter.representative_dataset = tf.lite.RepresentativeDataset(representative_dataset_gen)
-                """
+                    def representative_dataset_gen():
+                        for d in calibration_data[:5]:
+                            yield d
+                    converter.representative_dataset = tf.lite.RepresentativeDataset(representative_dataset_gen)
+                    """
 
-                # https://github.com/tensorflow/tensorflow/blob/61128913681a016033143fbe9b60140d983b3c98/tensorflow/lite/tools/optimize/quantize_model.cc
-                tflite_model = converter.convert()
-                open(tflite_model_file, "wb").write(tflite_model)
-                # """
+                    # https://github.com/tensorflow/tensorflow/blob/61128913681a016033143fbe9b60140d983b3c98/tensorflow/lite/tools/optimize/quantize_model.cc
+                    tflite_model = converter.convert()
+                    open(tflite_model_file, "wb").write(tflite_model)
+                    # """
 
-                obj = cls(
-                    component_config=meta,
-                    session=sess,
-                    label_list=label_list,
-                    predict_fn=predict_fn,
-                )
+                    obj = cls(
+                        component_config=meta,
+                        session=sess,
+                        label_list=label_list,
+                        predict_fn=predict_fn,
+                        use_tflite=True,
+                    )
 
-                obj.interpreter = tf.lite.Interpreter(model_path=tflite_model_file)
-                obj.interpreter.allocate_tensors()
+                    obj.interpreter = tf.lite.Interpreter(model_path=tflite_model_file)
+                    obj.interpreter.allocate_tensors()
 
-                obj.in_indices = {}
-                for i, in_name in enumerate(in_tensor_names):
-                    # print(in_name, obj.interpreter.get_input_details()[i]["shape"])
-                    obj.in_indices[in_name] = obj.interpreter.get_input_details()[i]["index"]
+                    obj.in_indices = {}
+                    for i, in_name in enumerate(in_tensor_names):
+                        # print(in_name, obj.interpreter.get_input_details()[i]["shape"])
+                        obj.in_indices[in_name] = obj.interpreter.get_input_details()[
+                            i
+                        ]["index"]
 
-                obj.out_index = obj.interpreter.get_output_details()[0]["index"]
+                    obj.out_index = obj.interpreter.get_output_details()[0]["index"]
+                else:
+                    obj = cls(
+                        component_config=meta,
+                        session=sess,
+                        label_list=label_list,
+                        predict_fn=predict_fn,
+                        use_tflite=False,
+                    )
 
                 return obj
-
         else:
             logger.warning(
                 "Failed to load nlu model. Maybe path {} "
