d1247a7cca63a99ecead36ed4df5d8b3972e4a0e
Ella <erohmensing@gmail.com>
2019-11-07 20:52:11 +0100

    fix CollectingOutputChannel send_response

--- rasa/core/channels/channel.py
+++ rasa/core/channels/channel.py
@@ -288,73 +288,20 @@ class CollectingOutputChannel(OutputChannel):
     def name(cls) -> Text:
         return "collector"
 
-    @staticmethod
-    def _message(
-        recipient_id: Text,
-        text: Text = None,
-        image: Text = None,
-        buttons: List[Dict[Text, Any]] = None,
-        attachment: Text = None,
-        custom: Dict[Text, Any] = None,
-    ) -> Dict:
-        """Create a message object that will be stored."""
-
-        obj = {
-            "recipient_id": recipient_id,
-            "text": text,
-            "image": image,
-            "buttons": buttons,
-            "attachment": attachment,
-            "custom": custom,
-        }
-
-        # filter out any values that are `None`
-        return utils.remove_none_values(obj)
-
     def latest_output(self) -> Optional[Dict[Text, Any]]:
         if self.messages:
             return self.messages[-1]
         else:
             return None
 
+    async def send_response(self, recipient_id: Text, message: Dict[Text, Any]) -> None:
+        message.update({"recipient_id": recipient_id})
+        await self._persist_message(message)
+
     async def _persist_message(self, message: Dict[Text, Any]) -> None:
-        self.messages.append(message)  # pytype: disable=bad-return-type
-
-    async def send_text_message(
-        self, recipient_id: Text, text: Text, **kwargs: Any
-    ) -> None:
-        for message_part in text.split("\n\n"):
-            await self._persist_message(self._message(recipient_id, text=message_part))
-
-    async def send_image_url(
-        self, recipient_id: Text, image: Text, **kwargs: Any
-    ) -> None:
-        """Sends an image. Default will just post the url as a string."""
-
-        await self._persist_message(self._message(recipient_id, image=image))
-
-    async def send_attachment(
-        self, recipient_id: Text, attachment: Text, **kwargs: Any
-    ) -> None:
-        """Sends an attachment. Default will just post as a string."""
-
-        await self._persist_message(self._message(recipient_id, attachment=attachment))
-
-    async def send_text_with_buttons(
-        self,
-        recipient_id: Text,
-        text: Text,
-        buttons: List[Dict[Text, Any]],
-        **kwargs: Any,
-    ) -> None:
-        await self._persist_message(
-            self._message(recipient_id, text=text, buttons=buttons)
-        )
-
-    async def send_custom_json(
-        self, recipient_id: Text, json_message: Dict[Text, Any], **kwargs: Any
-    ) -> None:
-        await self._persist_message(self._message(recipient_id, custom=json_message))
+        self.messages.append(
+            utils.remove_none_values(message)
+        )  # pytype: disable=bad-return-type
 
 
 class QueueOutputChannel(CollectingOutputChannel):
