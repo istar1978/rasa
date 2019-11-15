60b8e13da8295612d4bbef41b203c25198236d76
!whoami <kol@posteo.de>
2019-11-15 17:47:42 +0100

    dummy files

---
+++ rasa/dummy.py
@@ -0,0 +1,11 @@
+import warnings
+
+def print_a_warning():
+    warnings.warn("A useful message.")
+
+def print_two_warnings():
+    warnings.warn("Numero uno")
+    warnings.warn("Another one")
+
+def you_shall_not():
+    pass
